f1f7208346e0eddd67f3ed45c2927df8dcc3fc03
landbroken <445299258@qq.com>
2018-07-06 11:38:18 +0800

    排序算法修改和单元测试项目添加

---
+++ SearchTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SearchTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 在此输入测试代码
+		}
+
+	};
+}
