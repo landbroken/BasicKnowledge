4082f48a669f6d05737a6ca03eaf285087de638b
landbroken <445299258@qq.com>
2018-07-18 10:33:38 +0800

    修改LeetCode项目的654.cpp
    新增c语言写法

--- LeetCodeCpp/654.cpp
+++ LeetCodeCpp/654.cpp
@@ -10,6 +10,7 @@ using namespace std;
 https://leetcode-cn.com/problems/maximum-binary-tree/description/
 */
 
+#pragma region Cpp方法，未通过
 
 // Definition for a binary tree node.
 struct TreeNode {
@@ -27,13 +28,11 @@ public:
 		vector<int>::iterator left, 
 		vector<int>::iterator right)
 	{
-		TreeNode* p=node;
-		if (left == right)
+		if (right==nums.end())
 		{
-			//根节点当前点
-			p = new TreeNode(*left);
-			return p;
+			return nullptr;
 		}
+		TreeNode* p = node;
 
 		//找到根节点
 		vector<int>::iterator it;
@@ -63,9 +62,49 @@ public:
 		{
 			return nullptr;
 		}
-		m_root = Divide(m_root,nums,nums.begin(), nums.end());
+		//找到根节点
+		vector<int>::iterator it;
+		vector<int>::iterator itRoot;
+		for (it = nums.begin(); it != nums.end(); it++)
+		{
+			if (*it>*itRoot)
+			{
+				itRoot = it;
+			}
+		}
+		m_root = new TreeNode(*itRoot);
+		m_root->left = Divide(m_root,nums,nums.begin(), nums.end());
+		m_root->right = Divide(m_root, nums, nums.begin(), nums.end());
 		return m_root;
 	}
 private:
 	TreeNode* m_root;
 };
+
+#pragma endregion
+
+#pragma region c方法，提交通过
+
+//int findMaxElementIndex(int *nums, int numsSize) {
+//	int idx = 0;
+//	int max = 0;
+//	for (int i = 0; i < numsSize; i++) {
+//		if (nums[i] > max) {
+//			max = nums[i];
+//			idx = i;
+//		}
+//	}
+//	return idx;
+//}
+//
+//struct TreeNode* constructMaximumBinaryTree(int* nums, int numsSize) {
+//	if (numsSize == 0)return NULL;
+//	int idx = findMaxElementIndex(nums, numsSize);
+//	struct TreeNode *root = (struct TreeNode *) malloc(sizeof *root);
+//	root->val = nums[idx];
+//	root->left = constructMaximumBinaryTree(nums, idx);
+//	root->right = constructMaximumBinaryTree(nums + idx + 1, numsSize - idx - 1);
+//	return root;
+//}
+
+#pragma endregion
