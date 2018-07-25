db574c0ae4419e1c0857c93a92b9f3b245182b13
landbroken <445299258@qq.com>
2018-07-25 09:44:30 +0800

    Update TreeCpp
    新增霍夫曼树demo

--- TreeCpp/TreeCpp_main.cpp
+++ TreeCpp/TreeCpp_main.cpp
@@ -8,34 +8,19 @@
 //https://blog.csdn.net/ajay666/article/details/76736333
 //4、《数据结构》二叉树 —— 二叉树的建立和遍历（C++实现）
 //https://www.jianshu.com/p/6e739de0d206
+//5、哈夫曼树(二)之 C++详解 
+//http://www.cnblogs.com/skywang12345/p/3706821.html
 //
 #include "stdafx.h"
 #include "BiTree.h"
+#include "Huffman.h"
 using namespace std;
 
-int main()
+int BinaryTree_test()
 {
-	//BiTree a;
-	//string s,s2;
-	//s = "ABD##E#F##C##";
-	//s2 = "1234567";
-	//a.createBiTree(s2);
-	//cout << "前序遍历：" << endl;
-	//a.preOrder();
-	//cout << "中序遍历：" << endl;
-	//a.inOrder();
-	//cout << "后序遍历1：" << endl;
-	//a.postOrder();
-	//cout << "后序遍历2：" << endl;
-	//a.postOrder1();
-	//cout << "层序遍历：" << endl;
-	//a.levelOrder();
-	//cout << "树高：" << endl;
-	//cout << a.getHeight() << endl;
-
 	BinaryTree<int> tree(1);
 	auto root = tree.GetRoot();
-	tree.InsertLeftChild(tree.GetRoot(),2);
+	tree.InsertLeftChild(tree.GetRoot(), 2);
 	tree.InsertRightChild(tree.GetRoot(), 3);
 	tree.InsertLeftChild(tree.GetRoot()->left_node, 4);
 	tree.InsertRightChild(tree.GetRoot()->left_node, 5);
@@ -60,7 +45,12 @@ int main()
 	cout << "树高：" << endl;
 	cout << tree.GetHeight(tree.GetRoot()) << endl;
 
-	int searchValue[10] = { 
+	return 0;
+}
+
+int BinarySearchTree_test()
+{
+	int searchValue[10] = {
 		13,8,8,15,6,
 		14,9,9,25,25 };
 	BinaryTree<int> searchTree;
@@ -81,7 +71,7 @@ int main()
 	cout << searchTreeNear->value << endl;
 	cout << endl << "二叉搜索树Find不存在：" << endl;
 	auto searchTreeFind1 = searchTree.Find(7, searchTree.GetRoot());
-	if (searchTreeFind1==nullptr)
+	if (searchTreeFind1 == nullptr)
 	{
 		cout << "nullptr" << endl;
 	}
@@ -114,6 +104,43 @@ int main()
 	cout << endl << "Remove(15后的层序遍历：" << endl;
 	searchTree.LevelOrderTraverse();
 
+	return 0;
+}
+
+int HuffmanTest_test()
+{
+	int a[] = { 5,6,8,7,15 };
+	int i, ilen = sizeof(a) / (sizeof(a[0]));
+	Huffman<int>* tree = new Huffman<int>();
+
+	cout << "== 添加数组: ";
+	for (i = 0; i<ilen; i++)
+		cout << a[i] << " ";
+
+	tree->create(a, ilen);
+
+	cout << "\n== 前序遍历: ";
+	tree->preOrder();
+
+	cout << "\n== 中序遍历: ";
+	tree->inOrder();
+
+	cout << "\n== 后序遍历: ";
+	tree->postOrder();
+	cout << endl;
+
+	cout << "== 树的详细信息: " << endl;
+	tree->print();
+
+	// 销毁二叉树
+	tree->destroy();
+
+	return 0;
+}
+
+int main()
+{
+	HuffmanTest_test();
 	system("pause");
 	return 0;
 }
